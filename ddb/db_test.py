a3f6ece4003518a1309761799c5e2d90d758fe0f
Prahlad Vanvari <prahladvanvari@gmail.com>
2026-01-26 21:21:28 +0530

    DDB

--- ddb/db_test.py
+++ ddb/db_test.py
@@ -1,6 +1,6 @@
 import duckdb
 import pandas as pd
 
-conn = duckdb.connect(r"C:\Users\Prahlad\Desktop\NSE_DB\nse_db.ddb", read_only=True)
-df = conn.execute("SELECT * FROM 'expiry_2025-09-30'").fetch_df()
+conn = duckdb.connect(r"C:\Users\Prahlad\Desktop\db\historical_data.ddb", read_only=True)
+df = conn.execute("SELECT * FROM '2025-09-02'").fetch_df()
 print(df)
